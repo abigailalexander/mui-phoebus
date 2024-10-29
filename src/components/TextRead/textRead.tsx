3cde688a7273caba2fd6a995af521d3a2c5c594b
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-10-29 11:09:55 +0000

    Create basic text read component

---
+++ src/components/TextRead/textRead.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { styled, TextField, } from "@mui/material";
+
+const CustomTextField = styled(TextField)({
+    "& .MuiInputBase-input.Mui-disabled": {
+        WebkitTextFillColor: "rgba(29, 41, 69, 1)",
+        fontFamily: "monospace",
+        textAlign: "center",
+        verticalAlign: "center",
+        backgroundColor: "rgba(240, 240, 240, 255)",
+    },
+    "& .MuiInputBase-input.MuiOutlinedInput-input": {
+        padding: "0px",
+        fontFamily: "monospace",
+        fontSize: 12
+    },
+    "& .MuiInputBase-root.Mui-disabled": {
+        "& > fieldset": {
+            borderColor: "rgba(240, 240, 240, 255)",
+        }
+    }
+})
+
+type TextReadProps = {
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    value: string | number
+}
+
+export default function TextRead(props: TextReadProps) {
+    return (
+        <CustomTextField sx={{ width: props.w, height: props.h }} value={props.value} variant="outlined" disabled={true}></CustomTextField>
+    )
+}
