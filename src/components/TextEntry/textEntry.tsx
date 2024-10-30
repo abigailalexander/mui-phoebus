fbe8e7aad647580662c3709ca697dea6dccd2714
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-10-30 08:38:45 +0000

    Add basic text entry widget

---
+++ src/components/TextEntry/textEntry.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { styled, TextField, } from "@mui/material";
+
+const CustomTextField = styled(TextField)({
+    '& .MuiInputBase-input': {
+        disableUnderline: true,
+        backgroundColor: 'white',
+        border: '1px solid rgba(29, 41, 69)',
+        //borderColor: 'primary.main',
+        padding: "0px",
+        '&:focus': {
+            border: '1px solid blue',
+            backgroundColor: "rgba(255, 240, 0)"
+        },
+        fontSize: 14
+    },
+    '& .MuiInput-underline:after': {
+        borderBottom: "0px solid transparent"
+    }
+})
+
+type TextEntryProps = {
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    value: string | number
+}
+
+export default function TextEntry(props: TextEntryProps) {
+    return (
+        <CustomTextField sx={{ width: props.w, height: props.h }} defaultValue={props.value} variant="filled"></CustomTextField>
+    )
+}
