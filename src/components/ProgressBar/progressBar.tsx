92294f8fea184419a4ce4a279545f7d6d149e644
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-10-31 14:36:47 +0000

    Fix progress bar sizing and border

--- src/components/ProgressBar/progressBar.tsx
+++ src/components/ProgressBar/progressBar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Box, LinearProgress, linearProgressClasses, styled } from "@mui/material";
+import { diamondTheme } from "../../theme";
 
 const CustomLinearProgress = styled(LinearProgress)({
     height: 20,
@@ -8,11 +9,11 @@ const CustomLinearProgress = styled(LinearProgress)({
     [`&.${linearProgressClasses.colorPrimary}`]: {
         borderRadius: 5,
         backgroundColor: "white",
-        border: "3px solid white"
+        border: "2px solid white"
     },
     [`& .${linearProgressClasses.bar}`]: {
         borderRadius: 5,
-        backgroundColor: "palette.primary.main",
+        backgroundColor: diamondTheme.palette.primary.main,
     },
 });
 
@@ -28,8 +29,8 @@ export default function ProgressBar(props: ProgressBarProps) {
     // Could also possibly add a box shadow like you get in phoebus
     // TO DO LATER
     return (
-        <Box sx={{ width: props.w, height: props.h, border: "1px solid rgba(190, 190, 190)", borderRadius: 1 }}>
-            <CustomLinearProgress variant="determinate" value={props.value} />
+        <Box sx={{ width: props.w, height: props.h, border: "1px solid rgba(190, 190, 190)", borderRadius: 1, overflow: "visible" }}>
+            <CustomLinearProgress sx={{ width: props.w - 4, height: props.h - 4 }} variant="determinate" value={props.value} />
         </Box>
     );
 }
