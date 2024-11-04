a4573fe963adcad0349df4932307eaf5f484dcde
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-11-04 11:46:15 +0000

    add dcm camera screen

--- src/routes/dcmScreen.tsx
+++ src/routes/dcmScreen.tsx
@@ -1,27 +1,24 @@
 import * as React from "react";
 import ActionButton from "../components/ActionButton/actionButton";
-import ChoiceButton from "../components/ChoiceButton/choiceButton";
-import DropDown from "../components/DropDown/dropDown";
 import GroupBox from "../components/GroupBox/GroupBox";
 import Led from "../components/Led/led";
 import ProgressBar from "../components/ProgressBar/progressBar";
 import TextEntry from "../components/TextEntry/textEntry";
 import TextRead from "../components/TextRead/textRead";
-import Label from "../components/Label/label";
 import { Box } from "@mui/material";
 
-export default function DCMCameraScreen() {
+export default function DCMScreen() {
     return (
-        <Box sx={{ position: "absolute", left: 0, top: 0, width: 1280, height: 430 }}>
+        <Box sx={{ backgroundColor: "rgba(240, 240, 240)", position: "absolute", left: 0, top: 0, width: 1280, height: 430 }}>
             <GroupBox x={0} y={0} w={340} h={90} value={"Embedded"}>
                 <ActionButton x={50} y={5} w={40} h={20} value="STOP" openScreen={false}></ActionButton>
                 <ActionButton x={5} y={5} w={40} h={20} value="GEAR" openScreen={true}></ActionButton>
                 <ActionButton x={95} y={5} w={30} h={20} value="-" openScreen={false}></ActionButton>
                 <TextRead x={130} y={5} w={140} h={20} value="12.969"></TextRead>
                 <ActionButton x={275} y={5} w={30} h={20} value="+" openScreen={false}></ActionButton>
-                <TextEntry x={5} y={30} w={85} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={5} y={30} w={85} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={100} y={30} w={20} h={20} value={0}></Led>
-                <TextEntry x={130} y={30} w={140} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={130} y={30} w={140} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={280} y={30} w={20} h={20} value={1}></Led>
             </GroupBox>
             <GroupBox x={0} y={90} w={340} h={90} value={"Embedded_1"}>
@@ -30,9 +27,9 @@ export default function DCMCameraScreen() {
                 <ActionButton x={95} y={5} w={30} h={20} value="-" openScreen={false}></ActionButton>
                 <TextRead x={130} y={5} w={140} h={20} value="12.969"></TextRead>
                 <ActionButton x={275} y={5} w={30} h={20} value="+" openScreen={false}></ActionButton>
-                <TextEntry x={5} y={30} w={85} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={5} y={30} w={85} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={100} y={30} w={20} h={20} value={0}></Led>
-                <TextEntry x={130} y={30} w={140} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={130} y={30} w={140} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={280} y={30} w={20} h={20} value={1}></Led>
             </GroupBox>
             <GroupBox x={0} y={180} w={340} h={90} value={"Embedded_2"}>
@@ -41,9 +38,9 @@ export default function DCMCameraScreen() {
                 <ActionButton x={95} y={5} w={30} h={20} value="-" openScreen={false}></ActionButton>
                 <TextRead x={130} y={5} w={140} h={20} value="12.969"></TextRead>
                 <ActionButton x={275} y={5} w={30} h={20} value="+" openScreen={false}></ActionButton>
-                <TextEntry x={5} y={30} w={85} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={5} y={30} w={85} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={100} y={30} w={20} h={20} value={0}></Led>
-                <TextEntry x={130} y={30} w={140} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={130} y={30} w={140} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={280} y={30} w={20} h={20} value={1}></Led>
             </GroupBox>
             <GroupBox x={0} y={270} w={340} h={90} value={"Embedded_3"}>
@@ -52,9 +49,9 @@ export default function DCMCameraScreen() {
                 <ActionButton x={95} y={5} w={30} h={20} value="-" openScreen={false}></ActionButton>
                 <TextRead x={130} y={5} w={140} h={20} value="12.969"></TextRead>
                 <ActionButton x={275} y={5} w={30} h={20} value="+" openScreen={false}></ActionButton>
-                <TextEntry x={5} y={30} w={85} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={5} y={30} w={85} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={100} y={30} w={20} h={20} value={0}></Led>
-                <TextEntry x={130} y={30} w={140} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={130} y={30} w={140} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={280} y={30} w={20} h={20} value={1}></Led>
             </GroupBox>
             <GroupBox x={340} y={0} w={340} h={60} value={"Temperature"}>
@@ -92,10 +89,10 @@ export default function DCMCameraScreen() {
                 <TextRead x={35} y={10} w={100} h={20} value="12.969"></TextRead>
                 <ActionButton x={140} y={10} w={30} h={20} value="+" openScreen={false}></ActionButton>
                 <Led x={0} y={35} w={20} h={20} value={0}></Led>
-                <TextEntry x={25} y={35} w={120} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={25} y={35} w={120} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={150} y={35} w={20} h={20} value={1}></Led>
                 <ActionButton x={0} y={60} w={40} h={20} value="More" openScreen={true}></ActionButton>
-                <TextEntry x={45} y={60} w={80} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={45} y={60} w={80} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <ActionButton x={130} y={60} w={40} h={20} value="STOP" openScreen={false}></ActionButton>
             </GroupBox>
             <GroupBox x={680} y={120} w={205} h={120} value={"Embedded_4"}>
@@ -103,10 +100,10 @@ export default function DCMCameraScreen() {
                 <TextRead x={35} y={10} w={100} h={20} value="12.969"></TextRead>
                 <ActionButton x={140} y={10} w={30} h={20} value="+" openScreen={false}></ActionButton>
                 <Led x={0} y={35} w={20} h={20} value={0}></Led>
-                <TextEntry x={25} y={35} w={120} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={25} y={35} w={120} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={150} y={35} w={20} h={20} value={1}></Led>
                 <ActionButton x={0} y={60} w={40} h={20} value="More" openScreen={true}></ActionButton>
-                <TextEntry x={45} y={60} w={80} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={45} y={60} w={80} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <ActionButton x={130} y={60} w={40} h={20} value="STOP" openScreen={false}></ActionButton>
             </GroupBox>
             <GroupBox x={680} y={240} w={205} h={120} value={"Embedded_5"}>
@@ -114,10 +111,10 @@ export default function DCMCameraScreen() {
                 <TextRead x={35} y={10} w={100} h={20} value="12.969"></TextRead>
                 <ActionButton x={140} y={10} w={30} h={20} value="+" openScreen={false}></ActionButton>
                 <Led x={0} y={35} w={20} h={20} value={0}></Led>
-                <TextEntry x={25} y={35} w={120} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={25} y={35} w={120} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <Led x={150} y={35} w={20} h={20} value={1}></Led>
                 <ActionButton x={0} y={60} w={40} h={20} value="More" openScreen={true}></ActionButton>
-                <TextEntry x={45} y={60} w={80} h={20} value={"0.100"}></TextEntry>
+                <TextEntry x={45} y={60} w={80} h={20} value={"0.100"} disabled={false}></TextEntry>
                 <ActionButton x={130} y={60} w={40} h={20} value="STOP" openScreen={false}></ActionButton>
             </GroupBox>
             <GroupBox x={885} y={0} w={340} h={60} value={"Temperature_6"}>
