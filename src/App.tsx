87f4a1ac804430e5dcadd8e5a5033878140f6aae
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-11-05 14:07:04 +0000

    Add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import DCMCameraScreen from './routes/dcmCamera'
+import DCMScreen from './routes/dcmScreen'
+import CameraScreen from './routes/cameraScreen';
+import LowLevelScreen from './routes/lowLevelScreen';
+import DCMDiagramScreen from './routes/dcmDiagramScreen';
+import MotorStaticScreen from './routes/motorStaticScreen';
+import MuiDCMDiagramScreen from './routes/muiDcmDiagramScreen';
+import MuiLowLevelScreen from './routes/muiLowLevelScreen';
+import MuiDCMScreen from './routes/muiDcmScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<DCMCameraScreen />} />
+    [<Route path="/dcm" element={<DCMScreen />} />,
+    <Route path="/lowlevel" element={<LowLevelScreen />} />,
+    <Route path="/camera" element={<CameraScreen />} />,
+    <Route path="/dcmdiagram" element={<DCMDiagramScreen />} />,
+    <Route path="/motorstatic" element={<MotorStaticScreen />} />,
+    <Route path="/mui-lowlevel" element={<MuiLowLevelScreen />} />,
+    <Route path="/mui-dcmdiagram" element={<MuiDCMDiagramScreen />} />,
+    <Route path="/mui-dcm" element={<MuiDCMScreen />} />,
+    ]
   )
 );
 
