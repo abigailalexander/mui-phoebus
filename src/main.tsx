a6888c5f6b1c8912e6be5e8c710cd2662420d853
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-11-05 14:03:36 +0000

    Add default theme option

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import { ThemeProvider } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { diamondTheme } from './theme.ts'
+import { diamondTheme, muiTheme } from './theme.ts'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={diamondTheme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </StrictMode>,
