5abfba6e14fb28d89d92990aa63e1bae60e4892e
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-11-07 14:05:23 +0000

    Fix theme

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,22 @@ import { createTheme } from '@mui/material/styles';
 
 export const muiTheme = createTheme({
     palette: {
-        mode: import.meta.env.VITE_LIGHT_MODE ? "light" : "dark"
+        mode: import.meta.env.VITE_MUI_LIGHT_MODE === "light" ? "light" : "dark",
+        ...(import.meta.env.VITE_MUI_LIGHT_MODE === "light" ? {
+            primary: {
+                main: "rgba(29, 41, 69)",
+                light: "rgba(128, 255, 255)",
+                dark: "rgba(76, 100, 171)",
+                contrastText: "rgba(255, 255, 255)"
+            },
+        } : {
+            primary: {
+                main: "#90caf9",
+                light: "#e3f2fd",
+                dark: "rgba(76, 100, 171)",
+                contrastText: "rgba(29, 41, 69)"
+            },
+        })
     }
 });
 
