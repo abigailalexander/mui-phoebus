5bc80f331e5bb93e5f166288f6f9b3b409940c05
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-11-12 08:41:26 +0000

    Add dividers

--- src/routes/cameraScreen.tsx
+++ src/routes/cameraScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ActionButton, DropDown } from "../components/widgets";
+import { ActionButton, Divider, DropDown } from "../components/widgets";
 import { GroupBox } from "../components/widgets";
 import Led from "../components/Led/led";
 import { TextEntry } from "../components/widgets";
@@ -15,31 +15,41 @@ export default function CameraScreen() {
                 <Label x={0} y={0} w={135} h={20} value="Acquire"></Label>
                 <ChoiceButton x={140} y={0} w={115} h={20} value={["Done", "Acquire"]}></ChoiceButton>
                 <Led x={308} y={0} w={20} h={20} value={1}></Led>
+                <Divider x={2} y={22} w={373} h={1} ></Divider>
                 <Label x={0} y={25} w={135} h={20} value="Array Counter"></Label>
                 <TextEntry x={140} y={25} w={115} h={20} value={"0.000"} disabled={false}></TextEntry>
                 <TextRead x={260} y={25} w={115} h={20} value={"7757.000"}></TextRead>
+                <Divider x={2} y={47} w={373} h={1} ></Divider>
                 <Label x={0} y={50} w={135} h={20} value="Array Callbacks"></Label>
                 <ChoiceButton x={140} y={50} w={115} h={20} value={["Disable", "Enable"]}></ChoiceButton>
                 <Led x={308} y={50} w={20} h={20} value={1}></Led>
+                <Divider x={2} y={72} w={373} h={1} ></Divider>
                 <Label x={0} y={75} w={135} h={20} value="Acquire Time"></Label>
                 <TextEntry x={140} y={75} w={115} h={20} value={"0.000"} disabled={false}></TextEntry>
                 <TextRead x={260} y={75} w={115} h={20} value={"7757.000"}></TextRead>
+                <Divider x={2} y={97} w={373} h={1} ></Divider>
                 <Label x={0} y={100} w={135} h={20} value="Acquire Period"></Label>
                 <TextEntry x={140} y={100} w={115} h={20} value={"0.000"} disabled={false}></TextEntry>
                 <TextRead x={260} y={100} w={115} h={20} value={"7757.000"}></TextRead>
+                <Divider x={2} y={122} w={373} h={1} ></Divider>
                 <Label x={0} y={125} w={135} h={20} value="Num Images Counter"></Label>
                 <TextEntry x={140} y={125} w={235} h={20} value={"7757.000"} disabled={false}></TextEntry>
+                <Divider x={2} y={147} w={373} h={1} ></Divider>
                 <Label x={0} y={150} w={135} h={20} value="Num Images"></Label>
                 <TextEntry x={140} y={150} w={115} h={20} value={"0.000"} disabled={false}></TextEntry>
                 <TextRead x={260} y={150} w={115} h={20} value={"7757.000"}></TextRead>
+                <Divider x={2} y={172} w={373} h={1} ></Divider>
                 <Label x={0} y={175} w={135} h={20} value="Num Exposures"></Label>
                 <TextEntry x={140} y={175} w={115} h={20} value={"0.000"} disabled={false}></TextEntry>
                 <TextRead x={260} y={175} w={115} h={20} value={"7757.000"}></TextRead>
+                <Divider x={2} y={197} w={373} h={1} ></Divider>
                 <Label x={0} y={200} w={135} h={20} value="Image Mode"></Label>
                 <DropDown x={140} y={200} w={115} h={20} value={["Continuous", "Item 1", "Item 2", "Item 3",]}></DropDown>
                 <TextRead x={260} y={200} w={115} h={20} value={"Continuous"}></TextRead>
+                <Divider x={2} y={222} w={373} h={1} ></Divider>
                 <Label x={0} y={225} w={135} h={20} value="Detector State"></Label>
                 <TextEntry x={140} y={225} w={235} h={20} value={"Waiting"} disabled={false}></TextEntry>
+                <Divider x={2} y={247} w={373} h={1} ></Divider>
                 <Label x={0} y={250} w={135} h={20} value="Status Message"></Label>
                 <TextEntry x={140} y={250} w={235} h={20} value={"Acquiring Data"} disabled={false}></TextEntry>
             </GroupBox>
@@ -47,11 +57,14 @@ export default function CameraScreen() {
                 <Label x={0} y={0} w={135} h={20} value="N Dimensions"></Label>
                 <TextEntry x={140} y={0} w={115} h={20} value={"0.000"} disabled={false}></TextEntry>
                 <TextRead x={260} y={0} w={115} h={20} value={"7757.000"}></TextRead>
+                <Divider x={2} y={22} w={373} h={1} ></Divider>
                 <Label x={0} y={25} w={135} h={20} value="Dimensions"></Label>
                 <TextEntry x={140} y={25} w={115} h={20} value={"[]"} disabled={false}></TextEntry>
                 <TextRead x={260} y={25} w={115} h={20} value={"1280.00, 800.00"}></TextRead>
+                <Divider x={2} y={47} w={373} h={1} ></Divider>
                 <Label x={0} y={50} w={135} h={20} value="Unique ID"></Label>
                 <TextEntry x={140} y={50} w={235} h={20} value={"7756.0000"} disabled={false}></TextEntry>
+                <Divider x={2} y={72} w={373} h={1} ></Divider>
                 <Label x={0} y={75} w={135} h={20} value="Enable Callbacks"></Label>
                 <ChoiceButton x={140} y={75} w={115} h={20} value={["Disable", "Enable"]}></ChoiceButton>
                 <Led x={308} y={75} w={20} h={20} value={1}></Led>
--- src/routes/lowLevelScreen.tsx
+++ src/routes/lowLevelScreen.tsx
@@ -1,12 +1,7 @@
 import * as React from "react";
-import { ActionButton } from "../components/widgets";
-import { GroupBox } from "../components/widgets";
+import { ActionButton, Divider, DropDown, GroupBox, Label, TextEntry, TextRead } from "../components/widgets";
 import Led from "../components/Led/led";
-import { TextEntry } from "../components/widgets";
-import { TextRead } from "../components/widgets";
 import { Box } from "@mui/material";
-import { Label } from "../components/widgets";
-import { DropDown } from "../components/widgets";
 
 export default function LowLevelScreen() {
     return (
@@ -14,66 +9,92 @@ export default function LowLevelScreen() {
             <GroupBox x={0} y={25} w={325} h={331} value={"INPUTS"}>
                 <Label x={0} y={0} w={65} h={20} value="Label" ></Label>
                 <TextEntry x={70} y={0} w={205} h={20} value={"Sequencer"} disabled={false}></TextEntry>
+                <Divider x={10} y={22} w={265} h={1} ></Divider>
                 <Label x={0} y={25} w={65} h={20} value="Enable" ></Label>
                 <TextEntry x={70} y={25} w={205} h={20} value={"PCAP.ACTIVE"} disabled={false}></TextEntry>
+                <Divider x={10} y={47} w={265} h={1} ></Divider>
                 <Label x={0} y={50} w={65} h={20} value="Delay" ></Label>
                 <TextEntry x={70} y={50} w={205} h={20} value={"0"} disabled={false}></TextEntry>
+                <Divider x={10} y={72} w={265} h={1} ></Divider>
                 <Label x={0} y={75} w={65} h={20} value="Bita" ></Label>
                 <TextEntry x={70} y={75} w={205} h={20} value={"TTLIN1.VAL"} disabled={false}></TextEntry>
+                <Divider x={10} y={97} w={265} h={1} ></Divider>
                 <Label x={0} y={100} w={65} h={20} value="Delay" ></Label>
                 <TextEntry x={70} y={100} w={205} h={20} value={"0"} disabled={false}></TextEntry>
+                <Divider x={10} y={122} w={265} h={1} ></Divider>
                 <Label x={0} y={125} w={65} h={20} value="Bitb" ></Label>
                 <TextEntry x={70} y={125} w={205} h={20} value={"ZERO"} disabled={false}></TextEntry>
+                <Divider x={10} y={147} w={265} h={1} ></Divider>
                 <Label x={0} y={150} w={65} h={20} value="Delay" ></Label>
                 <TextEntry x={70} y={150} w={205} h={20} value={"0"} disabled={false}></TextEntry>
+                <Divider x={10} y={172} w={265} h={1} ></Divider>
                 <Label x={0} y={175} w={65} h={20} value="Bitc" ></Label>
                 <TextEntry x={70} y={175} w={205} h={20} value={"ZERO"} disabled={false}></TextEntry>
+                <Divider x={10} y={197} w={265} h={1} ></Divider>
                 <Label x={0} y={200} w={65} h={20} value="Delay" ></Label>
                 <TextEntry x={70} y={200} w={205} h={20} value={"0"} disabled={false}></TextEntry>
+                <Divider x={10} y={222} w={265} h={1} ></Divider>
                 <Label x={0} y={225} w={65} h={20} value="Posa" ></Label>
                 <TextEntry x={70} y={225} w={205} h={20} value={"INENC1.VAL"} disabled={false}></TextEntry>
+                <Divider x={10} y={247} w={265} h={1} ></Divider>
                 <Label x={0} y={250} w={65} h={20} value="Posb" ></Label>
                 <TextEntry x={70} y={250} w={205} h={20} value={"ZERO"} disabled={false}></TextEntry>
+                <Divider x={10} y={272} w={265} h={1} ></Divider>
                 <Label x={0} y={275} w={65} h={20} value="Posc" ></Label>
                 <TextEntry x={70} y={275} w={205} h={20} value={"ZERO"} disabled={false}></TextEntry>
             </GroupBox>
             <GroupBox x={0} y={361} w={325} h={336} value={"PARAMETERS"}>
+                <Divider x={10} y={202} w={265} h={1} ></Divider>
                 <Label x={0} y={205} w={65} h={20} value="Mode" ></Label>
                 <DropDown x={70} y={205} w={205} h={20} value={["VIEW", "EDIT", "SUBMIT", "DISCARD"]}></DropDown>
+                <Divider x={10} y={227} w={265} h={1} ></Divider>
                 <Label x={0} y={230} w={65} h={20} value="Prescale" ></Label>
                 <TextEntry x={70} y={230} w={205} h={20} value={"1 ms"} disabled={false}></TextEntry>
+                <Divider x={10} y={252} w={265} h={1} ></Divider>
                 <Label x={0} y={255} w={65} h={20} value="Units" ></Label>
                 <DropDown x={70} y={255} w={205} h={20} value={["min", "s", "ms", "us"]}></DropDown>
+                <Divider x={10} y={277} w={265} h={1} ></Divider>
                 <Label x={0} y={280} w={65} h={20} value="Repeats" ></Label>
                 <TextEntry x={70} y={280} w={205} h={20} value={"500"} disabled={false}></TextEntry>
             </GroupBox>
             <GroupBox x={325} y={25} w={335} h={206} value={"OUTPUTS"}>
                 <Label x={0} y={0} w={75} h={20} value="Active" ></Label>
                 <Led x={173} y={0} w={20} h={20} value={1}></Led>
+                <Divider x={11} y={22} w={275} h={1} ></Divider>
                 <Label x={0} y={25} w={75} h={20} value="Outa" ></Label>
                 <Led x={173} y={25} w={20} h={20} value={1}></Led>
+                <Divider x={11} y={47} w={275} h={1} ></Divider>
                 <Label x={0} y={50} w={75} h={20} value="Outb" ></Label>
                 <Led x={173} y={50} w={20} h={20} value={0}></Led>
+                <Divider x={11} y={72} w={275} h={1} ></Divider>
                 <Label x={0} y={75} w={75} h={20} value="Outc" ></Label>
                 <Led x={173} y={75} w={20} h={20} value={0}></Led>
+                <Divider x={11} y={97} w={275} h={1} ></Divider>
                 <Label x={0} y={100} w={75} h={20} value="Outd" ></Label>
                 <Led x={173} y={100} w={20} h={20} value={1}></Led>
+                <Divider x={11} y={122} w={275} h={1} ></Divider>
                 <Label x={0} y={125} w={75} h={20} value="Oute" ></Label>
                 <Led x={173} y={125} w={20} h={20} value={0}></Led>
+                <Divider x={11} y={147} w={275} h={1} ></Divider>
                 <Label x={0} y={150} w={75} h={20} value="Outf" ></Label>
                 <Led x={173} y={150} w={20} h={20} value={0}></Led>
             </GroupBox>
             <GroupBox x={325} y={236} w={335} h={183} value={"READBACKS"}>
                 <Label x={0} y={0} w={75} h={20} value="Table Repeat" ></Label>
                 <TextRead x={80} y={0} w={205} h={20} value={0}></TextRead>
+                <Divider x={11} y={22} w={275} h={1} ></Divider>
                 <Label x={0} y={25} w={75} h={20} value="Table Line" ></Label>
                 <TextRead x={80} y={25} w={205} h={20} value={0}></TextRead>
+                <Divider x={11} y={47} w={275} h={1} ></Divider>
                 <Label x={0} y={50} w={75} h={20} value="Line Repeat" ></Label>
                 <TextRead x={80} y={50} w={205} h={20} value={0}></TextRead>
+                <Divider x={11} y={72} w={275} h={1} ></Divider>
                 <Label x={0} y={75} w={75} h={20} value="State" ></Label>
                 <TextRead x={80} y={75} w={205} h={20} value={"WAIT_ENABLE"}></TextRead>
+                <Divider x={11} y={97} w={275} h={1} ></Divider>
                 <Label x={0} y={100} w={75} h={20} value="Can write" ></Label>
                 <TextRead x={80} y={100} w={205} h={20} value={0}></TextRead>
+                <Divider x={11} y={122} w={275} h={1} ></Divider>
                 <ActionButton x={11} y={125} w={275} h={20} value="All Position Capture Parameters ⧉" openScreen={true} fontSize={14}></ActionButton>
             </GroupBox>
             <Label x={375} y={419} w={65} h={20} value="Health" ></Label>
@@ -81,48 +102,68 @@ export default function LowLevelScreen() {
             <GroupBox x={660} y={25} w={500} h={300} value={"Heater 1"}>
                 <Label x={0} y={0} w={225} h={20} value="Heater 1 setpoint demand (D14):" fontSize={12}></Label>
                 <TextEntry x={230} y={0} w={230} h={20} value={"<0.0 C>"} disabled={true}></TextEntry>
+                <Divider x={0} y={23} w={460} h={1} ></Divider>
                 <Label x={0} y={25} w={225} h={20} value="Heater 1 setpoint readback (D153):" fontSize={12} ></Label>
                 <TextRead x={230} y={25} w={230} h={20} value={"<22.0C>"}></TextRead>
+                <Divider x={0} y={48} w={460} h={1} ></Divider>
                 <Label x={0} y={50} w={225} h={20} value="Heater 1 loop control (D10):" fontSize={12}></Label>
                 <ActionButton x={230} y={50} w={230} h={20} value={"Start Heating Loop"} openScreen={false}></ActionButton>
+                <Divider x={0} y={73} w={460} h={1} ></Divider>
                 <Label x={0} y={75} w={225} h={20} value="Heater 1 loop control readback (D11):" fontSize={12} ></Label>
                 <TextRead x={230} y={75} w={230} h={20} value={"<Reset Interlocks Request>"}></TextRead>
+                <Divider x={0} y={98} w={460} h={1} ></Divider>
                 <Label x={0} y={100} w={225} h={20} value="Heater 1 loop status (D12):" fontSize={12}></Label>
                 <TextRead x={230} y={100} w={230} h={20} value={"<Loop Disabled>"}></TextRead>
+                <Divider x={0} y={123} w={460} h={1} ></Divider>
                 <Label x={0} y={125} w={225} h={20} value="Heater 1 proportional gain (D150):" fontSize={12}></Label>
                 <TextEntry x={230} y={125} w={230} h={20} value={"0.000"} disabled={false}></TextEntry>
+                <Divider x={0} y={148} w={460} h={1} ></Divider>
                 <Label x={0} y={150} w={225} h={20} value="Heater 1 integral gain (D151):" fontSize={12} ></Label>
                 <TextEntry x={230} y={150} w={230} h={20} value={"0.000"} disabled={false}></TextEntry>
+                <Divider x={0} y={173} w={460} h={1} ></Divider>
                 <Label x={0} y={175} w={225} h={20} value="Heater 1 derivative gain (D152):" fontSize={12}></Label>
                 <TextEntry x={230} y={175} w={230} h={20} value={"0.000"} disabled={false}></TextEntry>
+                <Divider x={0} y={198} w={460} h={1} ></Divider>
                 <Label x={0} y={200} w={225} h={20} value="Heater 1 PID loop output 0-100% (D500):" fontSize={12} ></Label>
                 <TextRead x={230} y={200} w={230} h={20} value={"<Loop Disabled>"}></TextRead>
+                <Divider x={0} y={223} w={460} h={1} ></Divider>
                 <Label x={0} y={225} w={225} h={20} value="Heater 1 under temp setpoint (D122):" fontSize={12} ></Label>
                 <TextEntry x={230} y={225} w={230} h={20} value={"0.000"} disabled={false}></TextEntry>
+                <Divider x={0} y={248} w={460} h={1} ></Divider>
                 <Label x={0} y={250} w={225} h={20} value="Heater 1 fail alarm (D19):" fontSize={12}></Label>
                 <TextRead x={230} y={250} w={230} h={20} value={"<Status OK>"}></TextRead>
             </GroupBox>
             <GroupBox x={660} y={325} w={500} h={300} value={"Heater 2"}>
                 <Label x={0} y={0} w={225} h={20} value="Heater 2 setpoint demand (D14):" fontSize={12}></Label>
                 <TextEntry x={230} y={0} w={230} h={20} value={"<0.0 C>"} disabled={true}></TextEntry>
+                <Divider x={0} y={23} w={460} h={1} ></Divider>
                 <Label x={0} y={25} w={225} h={20} value="Heater 2 setpoint readback (D153):" fontSize={12}></Label>
                 <TextRead x={230} y={25} w={230} h={20} value={"<22.0C>"}></TextRead>
+                <Divider x={0} y={48} w={460} h={1} ></Divider>
                 <Label x={0} y={50} w={225} h={20} value="Heater 2 loop control (D10):" fontSize={12}></Label>
                 <ActionButton x={230} y={50} w={230} h={20} value={"Start Heating Loop"} openScreen={false}></ActionButton>
+                <Divider x={0} y={73} w={460} h={1} ></Divider>
                 <Label x={0} y={75} w={225} h={20} value="Heater 2 loop control readback (D11):" fontSize={12} ></Label>
                 <TextRead x={230} y={75} w={230} h={20} value={"<Reset Interlocks Request>"}></TextRead>
+                <Divider x={0} y={98} w={460} h={1} ></Divider>
                 <Label x={0} y={100} w={225} h={20} value="Heater 2 loop status (D12):" fontSize={12}></Label>
                 <TextRead x={230} y={100} w={230} h={20} value={"<Loop Disabled>"}></TextRead>
+                <Divider x={0} y={123} w={460} h={1} ></Divider>
                 <Label x={0} y={125} w={225} h={20} value="Heater 2 proportional gain (D150):" fontSize={12}></Label>
                 <TextEntry x={230} y={125} w={230} h={20} value={"0.000"} disabled={false}></TextEntry>
+                <Divider x={0} y={148} w={460} h={1} ></Divider>
                 <Label x={0} y={150} w={225} h={20} value="Heater 2 integral gain (D151):" fontSize={12}></Label>
                 <TextEntry x={230} y={150} w={230} h={20} value={"0.000"} disabled={false}></TextEntry>
+                <Divider x={0} y={173} w={460} h={1} ></Divider>
                 <Label x={0} y={175} w={225} h={20} value="Heater 2 derivative gain (D152):" fontSize={12}></Label>
                 <TextEntry x={230} y={175} w={230} h={20} value={"0.000"} disabled={false}></TextEntry>
+                <Divider x={0} y={198} w={460} h={1} ></Divider>
                 <Label x={0} y={200} w={225} h={20} value="Heater 2 PID loop output 0-100% (D500):" fontSize={12}></Label>
                 <TextRead x={230} y={200} w={230} h={20} value={"<Loop Disabled>"}></TextRead>
+                <Divider x={0} y={223} w={460} h={1} ></Divider>
                 <Label x={0} y={225} w={225} h={20} value="Heater 2 under temp setpoint (D122):" fontSize={12}></Label>
                 <TextEntry x={230} y={225} w={230} h={20} value={"0.000"} disabled={false}></TextEntry>
+                <Divider x={0} y={248} w={460} h={1} ></Divider>
                 <Label x={0} y={250} w={225} h={20} value="Heater 2 fail alarm (D19):" fontSize={12}></Label>
                 <TextRead x={230} y={250} w={230} h={20} value={"<Status OK>"}></TextRead>
             </GroupBox>
