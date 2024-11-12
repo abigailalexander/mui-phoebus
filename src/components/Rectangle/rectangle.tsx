d8ef43a03e36ca5aab37e9b7d9a0e5d4265b5216
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-11-12 11:28:27 +0000

    Change divider color in light and dark mode

--- src/components/Rectangle/rectangle.tsx
+++ src/components/Rectangle/rectangle.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
-import { Divider } from "@mui/material";
+import { Divider, useTheme } from "@mui/material";
 import { BaseProps } from "../../utils/baseProps";
 
 
 export default function MuiDivider(props: BaseProps) {
+    const palette = useTheme().palette;
+    const bgColor = import.meta.env.VITE_MUI_LIGHT_MODE === "true" ? "rgba(184, 195, 223)" : palette.primary.contrastText;
     const groupShift = props.inGroup ? 8 : 0;
 
     return (
-        <Divider sx={{ left: props.x + groupShift, top: props.y, width: props.w, position: "absolute", backgroundColor: "rgba(184, 195, 223)" }} />
+        <Divider sx={{ left: props.x + groupShift, top: props.y, width: props.w, position: "absolute", backgroundColor: bgColor }} />
     )
 }
